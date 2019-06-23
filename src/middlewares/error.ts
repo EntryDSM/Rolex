0a6710bca1be165d8802e059468d0349082d62b7
huewilliams <4879sinwoo@naver.com>
2019-06-23 23:37:29 +0900

    EDIT tslint 에 맞춰 코드를 변경함

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,15 +1,15 @@
-import { NextFunction, Request, Response } from "express";
-import HttpException from '../exceptions/HttpException';
+import { NextFunction, Request, Response } from 'express'
+import HttpException from '../exceptions/HttpException'
 
-function errorMiddleware(error:HttpException, req:Request, res:Response, next:NextFunction) {
-    const status = error.status || 500;
-    const message = error.message || 'server error';
-    res
+function errorMiddleware (error: HttpException, req: Request, res: Response, next: NextFunction) {
+  const status = error.status || 500
+  const message = error.message || 'server error'
+  res
         .status(status)
         .send({
-            message,
-            status
-        });
+          message,
+          status
+        })
 }
 
-export default errorMiddleware;
+export default errorMiddleware
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import App from "./app";
+import App from './app'
 
-const app = new App();
+const app = new App()
 
-app.listen(3000);
+app.listen(3000)
