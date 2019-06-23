5f36c1a24ff2b407bb61c496a9133c2f34621fe6
huewilliams <4879sinwoo@naver.com>
2019-06-23 23:33:01 +0900

    SET project setting

---
+++ src/middlewares/error.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import HttpException from '../exceptions/HttpException';
+
+function errorMiddleware(error:HttpException, req:Request, res:Response, next:NextFunction) {
+    const status = error.status || 500;
+    const message = error.message || 'server error';
+    res
+        .status(status)
+        .send({
+            message,
+            status
+        });
+}
+
+export default errorMiddleware;
