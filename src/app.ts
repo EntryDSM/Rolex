281325c57ebdbcee192cb5b44d21dddbc35f0553
huewilliams <4879sinwoo@naver.com>
2019-08-24 23:01:23 +0900

    FEATURE: vault reconnection batch job

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,51 @@
 import express from 'express'
+import nodeSchedule from 'node-schedule';
 import errorMiddleware from './middlewares/error'
-import VaultClient from './utils/vaultClient'
-import RedisClient from './utils/redisClient'
+import { connectToVault, ConnectToRedis } from './utils/connectService';
+import VaultData from './interfaces/VaultData';
+import RedisClient from './utils/redisClient';
 
 class App {
   public app: express.Application
-  private vaultClient?: VaultClient
-  private redisClient?: RedisClient
 
-  constructor () {
+  constructor() {
     this.app = express()
     this.initializeMiddleware()
     this.initializeErrorHandler()
   }
 
-  public async listen (port: number) {
-    if (!this.redisClient) {
-      this.vaultClient = new VaultClient()
-      this.redisClient = new RedisClient(await this.vaultClient.connectVault())
-    }
+  public listen(port: number, listener: () => void) {
     this.app.set('PORT', port)
     this.app.listen(this.app.get('PORT'), () => {
       console.log(`server is listening on ${this.app.get('PORT')}`)
     })
   }
 
-  private initializeMiddleware (): void {
+  private initializeMiddleware(): void {
     this.app.use(express.json())
     this.app.use(express.urlencoded({ extended: false }))
   }
 
-  private initializeErrorHandler (): void {
+  private initializeErrorHandler(): void {
     this.app.use(errorMiddleware)
-  }
-
-  public async getRedisClient (): Promise<RedisClient> {
-    if (!this.redisClient) {
-      this.vaultClient = new VaultClient()
-      this.redisClient = new RedisClient(await this.vaultClient.connectVault())
-    }
-    return this.redisClient
   }
 }
 
-export default App
+const createApp = async () => {
+  connectServices();
+  console.log(new Date().getMinutes());
+  const startMinute = new Date().getMinutes();
+  const rule = new nodeSchedule.RecurrenceRule();
+  rule.minute = startMinute - 1;
+  const batchJob = nodeSchedule.scheduleJob(rule, () => {
+    connectServices();
+  })
+  return new App();
+};
+
+const connectServices = async () =>{
+  const vaultConnection:VaultData = await connectToVault();
+  const redisConnection:RedisClient = await ConnectToRedis(vaultConnection);
+}
+
+export default createApp;
