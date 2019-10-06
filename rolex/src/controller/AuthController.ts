cfa71ee82ef8a631ccaa4a148e64bdc31268e5ff
huewilliams <4879sinwoo@naver.com>
2019-10-06 11:46:59 +0900

    FIX: error handling

--- rolex/src/controller/AuthController.ts
+++ rolex/src/controller/AuthController.ts
@@ -1,49 +1,57 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 import { getRepository, getConnectionManager } from "typeorm";
 import { validate } from "class-validator";
 import { Admin } from "../entity/Admin"
 import { jwtSecret } from '../config/config';
 import { redisClient } from '../utils/redisWrapper';
+import Err from "../middleware/errorHandlers";
 
 class AuthController {
-    static login = async (req: Request, res: Response)=>{
-        let { id, password } = req.body;
-        if(!(id && password)) {
-            res.status(400).send("Invalid Request Format");
-        }   
-
-        const manager = getConnectionManager().get('rolex');
-        const adminRepository = manager.getRepository(Admin);
-        let admin: Admin;
+    static login = async (req: Request, res: Response, next: NextFunction)=>{
         try {
-            admin = await adminRepository.findOneOrFail({where: {admin_id: id}});
-        } catch (error) {
-            res.status(401).send("Invalid Id or Password");
-        }
+            let { id, password } = req.body;
+            if(!(id && password)) {
+                throw new Err('invalid login info');
+            }   
+    
+            const manager = getConnectionManager().get('rolex');
+            const adminRepository = manager.getRepository(Admin);
+            let admin: Admin;
 
-        if (!admin.checkPassword(password)) {
-            res.status(401).send("Invalid Id or Password");
-            return;
-        } else {
-            const AcessToken = jwt.sign(
-                { adminId: admin.admin_id },
-                jwtSecret,
-                { expiresIn: "60m" }
-            );
-            const RefreshToken = jwt.sign(
-                { adminId: admin.admin_id, adminType: admin.admin_type },
-                jwtSecret,
-                { expiresIn: "12h" }
-            );
-            
-            await redisClient.set(`rolex_${id}`, RefreshToken);
-            
-            res.json({
-                access: AcessToken,
-                refresh: RefreshToken,
-            });
+            admin = await adminRepository.findOneOrFail({where: {admin_id: id}});
+            if(!admin) throw new Err('invalid login info');
+
+            if (!admin.checkPassword(password)) {
+                throw new Err('invalid login info');
+            } else {
+                const AcessToken = jwt.sign(
+                    { adminId: admin.admin_id },
+                    jwtSecret,
+                    { expiresIn: "60m" }
+                );
+                const RefreshToken = jwt.sign(
+                    { adminId: admin.admin_id, adminType: admin.admin_type },
+                    jwtSecret,
+                    { expiresIn: "12h" }
+                );
+                
+                await redisClient.set(`rolex_${id}`, RefreshToken, 'EX', 60*60*12);
+                
+                res.json({
+                    access: AcessToken,
+                    refresh: RefreshToken,
+                });
+            }   
+        } catch (e) {
+            e.status = 403;
+            e.code = 1200
+            next(e);
         }
+    }
+
+    static refresh = async (req: Request, res: Response) => {
+
     }
 }
 
