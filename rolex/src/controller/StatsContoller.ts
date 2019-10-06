8efe9783f6e282522301644d6790fbb59f17b2e3
huewilliams <4879sinwoo@naver.com>
2019-10-06 14:36:08 +0900

    ADD: GET /stats/all API

--- rolex/src/controller/StatsContoller.ts
+++ rolex/src/controller/StatsContoller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { getConnectionManager } from "typeorm";
+import { getConnectionManager, getRepository } from "typeorm";
 import { Ged_application } from "../entity/Ged";
 import { Graduated_application } from "../entity/Graduated";
 import { Ungraduated_application } from "../entity/Ungraduated";
@@ -35,6 +35,57 @@ class StatsController {
         } catch(e) {
             next(e);
         }
+    }
+
+    static getAll = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            let dmc = 0, dsc = 0, dcc = 0, nmc = 0, nsc = 0, ncc = 0;
+            dmc = await StatsController.find(true, "MEISTER");
+            dsc = await StatsController.find(true, "SOCIAL");
+            dcc = await StatsController.find(true, "COMMON");
+            nmc = await StatsController.find(false, "MEISTER");
+            nsc = await StatsController.find(false, "SOCIAL");
+            ncc = await StatsController.find(false, "COMMON");
+
+            res.json({
+                daejeon: {
+                    meister: dmc,
+                    social: dsc,
+                    common: dcc
+                },
+                nation: {
+                    meister: nmc,
+                    social: nsc,
+                    common: ncc
+                },
+                total: {
+                    daejeon: dmc+dsc+dcc,
+                    nation: nmc+nsc+ncc
+                }
+            })
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    static find = async (is_daejeon:boolean, type:string) => {
+        const manager = getConnectionManager().get('avengers');
+            
+        const gedRepository = manager.getRepository(Ged_application);
+        const graduatedRepository = manager.getRepository(Graduated_application);
+        const ungraduatedRepository = manager.getRepository(Ungraduated_application);
+        
+        let data = {ged:null, gred:null, ungred:null};
+        data.ged = await gedRepository.find({
+            where: {is_daejeon:is_daejeon, apply_type: type}
+        });
+        data.gred = await graduatedRepository.find({
+            where: {is_daejeon:is_daejeon, apply_type: type}
+        });
+        data.ungred = await ungraduatedRepository.find({
+            where: {is_daejeon:is_daejeon, apply_type: type}
+        })
+        return data.ged.length+data.gred.length+data.ungred.length;
     }
 }
 
