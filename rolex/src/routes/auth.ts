674806d2df539160984516ac9db26d9c73d8838b
huewilliams <4879sinwoo@naver.com>
2019-10-06 12:09:55 +0900

    ADD: refresh router

--- rolex/src/routes/auth.ts
+++ rolex/src/routes/auth.ts
@@ -1,10 +1,13 @@
 import { Router } from "express"
 import AdminController from "../controller/AdminController";
 import AuthController from '../controller/AuthController';
+import { checkRefresh } from '../middleware/checkJwt' 
 
 const router = Router();
 
 router.post("/login", AuthController.login);
+
+router.patch("/refresh", checkRefresh, AuthController.refresh);
 
 router.get("/", AdminController.listAll);
 
