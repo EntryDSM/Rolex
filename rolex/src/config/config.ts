4e82c406c5d395b1be3c81c103cd064ac30158b7
huewilliams <4879sinwoo@naver.com>
2019-10-06 11:18:44 +0900

    ADD: redis config to config.js

--- rolex/src/config/config.ts
+++ rolex/src/config/config.ts
@@ -1,2 +1,5 @@
 export const port = Number(process.env.PORT) || 3000;
 export const jwtSecret = process.env.JWTSECRET;
+export const redisHost = process.env['REDIS_HOST'];
+export const redisPassword = process.env['REDIS_PASSWORD'];
+export const redisPort = Number(process.env['REDIS_PORT']);
