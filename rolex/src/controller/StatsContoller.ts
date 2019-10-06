5c8ef2d620cb4244b49eb1ea2177b3f05174934a
huewilliams <4879sinwoo@naver.com>
2019-10-06 15:09:29 +0900

    ADD: GET /stats/competition API

--- rolex/src/controller/StatsContoller.ts
+++ rolex/src/controller/StatsContoller.ts
@@ -87,6 +87,38 @@ class StatsController {
         })
         return data.ged.length+data.gred.length+data.ungred.length;
     }
+
+    static getCompetition = async (req: Request, res: Response, next: NextFunction) => {
+       try {
+            let dmc = 0, dsc = 0, dcc = 0, nmc = 0, nsc = 0, ncc = 0;
+            dmc = await StatsController.find(true, "MEISTER");
+            dsc = await StatsController.find(true, "SOCIAL");
+            dcc = await StatsController.find(true, "COMMON");
+            nmc = await StatsController.find(false, "MEISTER");
+            nsc = await StatsController.find(false, "SOCIAL");
+            ncc = await StatsController.find(false, "COMMON");
+            
+            res.json({
+                daejeon: {
+                    meister: `${(dmc/12).toFixed(1)}:1`,
+                    social: `${(dsc/2).toFixed(1)}:1`,
+                    common: `${(dcc/18).toFixed(1)}:1`
+                },
+                nation: {
+                    meister: `${(nmc/16).toFixed(1)}:1`,
+                    social: `${(nsc/2).toFixed(1)}:1`,
+                    common: `${(ncc/30).toFixed(1)}:1`
+                },
+                total: {
+                    daejeon: `${((dmc+dsc+dcc)/32).toFixed(1)}:1`,
+                    nation: `${((nmc+nsc+ncc)/48).toFixed(1)}:1`,
+                    all: `${((dmc+dsc+dcc+nmc+nsc+ncc)/80).toFixed(1)}:1`
+                }
+            })
+        } catch(e) {
+            next(e);
+        }
+    }
 }
 
 export default StatsController;
