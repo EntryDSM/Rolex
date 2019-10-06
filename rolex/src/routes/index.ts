705bac1ab1ffe7e5128f6af37aefb57910cd8e05
huewilliams <4879sinwoo@naver.com>
2019-10-06 13:55:47 +0900

    ADD: info router

--- rolex/src/routes/index.ts
+++ rolex/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response} from "express";
 import auth from './auth';
 import stats from './stats';
 import submit from './submit';
+import info from './info';
 
 const routes = Router();
 
 routes.use("/auth", auth);
 routes.use("/stats", stats);
 routes.use("/submit", submit);
+routes.use("/info", info);
 
 export default routes;
