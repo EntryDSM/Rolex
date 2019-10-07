b73332b7adbe07608982a126718ec6b02db39440
huewilliams <4879sinwoo@naver.com>
2019-10-07 11:49:19 +0900

    FIX: 필요없는 라우터 등록 제거

--- rolex/src/routes/auth.ts
+++ rolex/src/routes/auth.ts
@@ -1,5 +1,4 @@
 import { Router } from "express"
-import AdminController from "../controller/AdminController";
 import AuthController from '../controller/AuthController';
 import { checkRefresh } from '../middleware/checkJwt' 
 
@@ -8,9 +7,5 @@ const router = Router();
 router.post("/login", AuthController.login);
 
 router.patch("/refresh", checkRefresh, AuthController.refresh);
-
-router.get("/", AdminController.listAll);
-
-router.post("/", AdminController.newAdmin);
 
 export default router;
