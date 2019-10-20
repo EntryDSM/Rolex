95919374f13f84c7cd8bbba695aa5c0f9ccf7dc9
huewilliams <4879sinwoo@naver.com>
2019-10-21 00:12:24 +0900

    ADD: admin, root 판단 middleware 구현

--- rolex/src/middleware/checkJwt.ts
+++ rolex/src/middleware/checkJwt.ts
@@ -3,13 +3,14 @@ import * as jwt from "jsonwebtoken";
 import { jwtSecret } from "../config/config";
 import { redisClient } from "../utils/redisWrapper";
 import Err from "./errorHandlers";
+import { AdminType } from "../entity/Admin";
 
 export const checkRefresh = async (req: Request, res: Response, next: NextFunction) => {
     const token = <string>req.get("X-Refresh-Token");
     let jwtPayload;
     try {
         jwtPayload = jwt.verify(token, jwtSecret);
-        if(await redisClient.get(`rolex_${jwtPayload.admin_id}`)) {
+        if(await redisClient.get(`rolex:token_${jwtPayload.admin_id}`)) {
             res.locals.jwtPayload = jwtPayload;
             next();
         } else {
@@ -18,6 +19,37 @@ export const checkRefresh = async (req: Request, res: Response, next: NextFuncti
     } catch(e) {
         e.status = 401;
         e.code = 1202;
+        next(e);
+    }
+}
+
+export const checkAdmin = async (req: Request, res: Response, next: NextFunction) => {
+    const token = <string>req.get('Authorization');
+    let jwtPayload;
+    try {
+        jwtPayload = jwt.verify(token, jwtSecret);
+        if(jwtPayload) next();
+    } catch (e) {
+        e.status = 401;
+        e.code = 1202;
+        next(e);
+    }
+}
+
+export const checkRoot = async (req: Request, res: Response, next: NextFunction) => {
+    const token = <string>req.get('Authorization');
+    let jwtPayload;
+    try {
+        jwtPayload = jwt.verify(token, jwtSecret);
+        console.log(jwtPayload.adminType);
+        if(jwtPayload.adminType === AdminType.ROOT) {
+            next();
+        } else {
+            throw new Err('Access denied. please access with root account.')
+        }
+    } catch (e) {
+        e.status = 401;
+        e.code = 1203;
         next(e);
     }
 }
