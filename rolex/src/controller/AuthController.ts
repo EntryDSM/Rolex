c3b467311c6326aab5b8d752a86835b5328a1ffd
huewilliams <4879sinwoo@naver.com>
2019-10-21 00:10:51 +0900

    ADD: admin signup API 추가

--- rolex/src/controller/AuthController.ts
+++ rolex/src/controller/AuthController.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import * as jwt from "jsonwebtoken";
 import { getConnectionManager } from "typeorm";
 import { validate } from "class-validator";
-import { Admin } from "../entity/Admin"
+import { Admin, AdminType } from "../entity/Admin"
 import { jwtSecret } from '../config/config';
 import { redisClient } from '../utils/redisWrapper';
 import Err from "../middleware/errorHandlers";
@@ -26,7 +26,7 @@ class AuthController {
                 throw new Err('invalid login info');
             } else {
                 const AcessToken = jwt.sign(
-                    { adminId: admin.admin_id },
+                    { adminId: admin.admin_id, adminType: admin.admin_type },
                     jwtSecret,
                     { expiresIn: "60m" }
                 );
@@ -36,7 +36,7 @@ class AuthController {
                     { expiresIn: "12h" }
                 );
                 
-                await redisClient.set(`rolex_${id}`, RefreshToken, 'EX', 60*60*12);
+                await redisClient.set(`rolex:token_${id}`, RefreshToken, 'EX', 60*60*12);
                 
                 res.json({
                     access: AcessToken,
@@ -51,12 +51,47 @@ class AuthController {
     }
 
     static refresh = async (req: Request, res: Response) => {
-        const { admin_id } = res.locals.jwtPayload;
+        const { admin_id, admin_type } = res.locals.jwtPayload;
         const newToken = jwt.sign(
-            { adminId: admin_id },
+            { adminId: admin_id, admin_type },
             jwtSecret,
             { expiresIn: "60m" });
         res.status(201).json({access: newToken});
+    }
+
+    static newUser = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { id, password, email, type, name } = req.body;
+
+            if(!id || !password || !email || !name) {
+                throw new Err('user data must be provided');
+            } else {
+                const manager = getConnectionManager().get('rolex');
+                const adminRepository = manager.getRepository(Admin);
+                if(await adminRepository.findOne({where: {admin_id: id}})) {
+                    throw new Err('user already exists');
+                } else {
+                    let admin_type: AdminType;
+                    if(type === "root") {
+                        admin_type = AdminType.ROOT;
+                    } else {
+                        admin_type = AdminType.ADMINISTRATION;
+                    }
+                    let user = new Admin();
+                    user.admin_name = name;
+                    user.admin_type = admin_type;
+                    user.admin_id = id;
+                    user.admin_email = email;
+                    user.admin_password = password;
+                    user.hashPassword();
+                    await adminRepository.save(user);
+                    res.send('sign up success');
+                }
+            }  
+        } catch(e) {
+            e.code = 1301;
+            next(e);
+        }
     }
 }
 
